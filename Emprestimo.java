7db824b973a23f346cf5c801f7146fc0f55a3ae6
Heindell <hudsonrangel1@gmail.com>
2017-03-01 11:35:12 -0300

    Add files via upload

---
+++ Emprestimo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/**
+ * programa para checar se a pessoa pode receber o credito requisitado
+ * @author Hudson
+ *
+ */
+public class Emprestimo {
+
+	public static void main(String[] args) {
+		double salario, prestacao;
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Insira o salario bruto.");
+		salario = leitor.nextDouble();
+		System.out.println("Insira o valor da prestacao que foi pedido");
+		prestacao = leitor.nextDouble();
+		if (prestacao <= (salario*0.3)){
+			System.out.println("Emprestimo pode ser concedido");
+			
+		}
+		else{
+			System.out.println("Emprestimo recusado.");
+		}
+		leitor.close();
+		
+	}
+}
